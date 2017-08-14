abe9801fb381aa2cd96f9103254738f6c8583a6c
Василий Олехов <olekhov@gmail.com>
2017-08-14 18:19:39 +0300

    Added class for diary

---
+++ dnevnik.py
@@ -0,0 +1,15 @@
+#!env python3
+
+class Dnevnik:
+    """ Class to access electronic diary """
+    def __init__(self, pgu_auth):
+        """ pgu_auth: authenticator class for PGU """
+        self.auth=pgu_auth
+
+    def Authenticate():
+        if !self.auth.authenticated:
+            self.auth.Authenticate()
+
+    def SelectDiary(num):
+
+        E_DIARY[num]
